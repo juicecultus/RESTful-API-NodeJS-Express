f98af1153736e7976a2b0ca101d1be3e97cfd087
juicecultus <juicecultus@gmail.com>
2020-02-18 09:24:40 +0000

    handle PUT requests and refactor validation code

--- index.js
+++ index.js
@@ -19,15 +19,9 @@ app.get('/api/courses', (req, res) => {
 });
 
 app.post('/api/courses', (req, res) => {
-  const schema = Joi.object({
-    name: Joi.string()
-      .min(3)
-      .required()
-  });
-
-  const result = schema.validate(req.body);
-  if (result.error) {
-    res.status(400).send(result.error.details[0].message);
+  const { error } = validateCourse(req.body);
+  if (error) {
+    res.status(400).send(error.details[0].message);
     return;
   }
 
@@ -38,6 +32,31 @@ app.post('/api/courses', (req, res) => {
   courses.push(course);
   res.send(course);
 });
+
+app.put('/api/courses/:id', (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course)
+    res.status(404).send('The course with the given ID was not found.');
+
+  const { error } = validateCourse(req.body);
+  if (error) {
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+
+  course.name = req.body.name;
+  res.send(course);
+});
+
+function validateCourse(course) {
+  const schema = Joi.object({
+    name: Joi.string()
+      .min(3)
+      .required()
+  });
+
+  return schema.validate(course);
+}
 
 app.get('/api/courses/:id', (req, res) => {
   const course = courses.find(c => c.id === parseInt(req.params.id));
