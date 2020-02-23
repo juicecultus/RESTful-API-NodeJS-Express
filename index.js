e4030747d15ad5d8dbdd5a0fded4f26da130e2af
juicecultus <juicecultus@gmail.com>
2020-02-23 22:44:29 +0000

    refactor code

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const Joi = require('@hapi/joi');
-Joi.objectId = require('joi-objectid')(Joi);
+const logger = require('./startup/logging');
 const express = require('express');
 const app = express();
 
@@ -7,7 +6,8 @@ require('./startup/logging');
 require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/config')();
+require('./startup/validation');
 
 // Set port dynamically based on environment
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+app.listen(port, () => logger.info(`Listening on port ${port}...`));
