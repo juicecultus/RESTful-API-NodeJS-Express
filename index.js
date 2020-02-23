9479864f3fb491ecc9bb4d2a735d741a3980480d
juicecultus <juicecultus@gmail.com>
2020-02-23 21:13:53 +0000

    refactor code - move routes to module

--- index.js
+++ index.js
@@ -2,23 +2,16 @@ require('express-async-errors');
 require('winston-mongodb');
 const winston = require('./config/winston');
 const logger = require('./config/winston');
-const error = require('./middleware/error');
-const debug = require('debug')('app:startup');
 const config = require('config');
 const morgan = require('morgan');
-const helmet = require('helmet');
+const debug = require('debug')('app:startup');
 const Joi = require('@hapi/joi');
 Joi.objectId = require('joi-objectid')(Joi);
 const mongoose = require('mongoose');
-const genres = require('./routes/genres');
-const customers = require('./routes/customers');
-const movies = require('./routes/movies');
-const rentals = require('./routes/rentals');
-const users = require('./routes/users');
-const auth = require('./routes/auth');
-const home = require('./routes/home');
 const express = require('express');
 const app = express();
+
+require('./startup/routes')(app);
 
 process.on('uncaughtException', ex => {
   logger.error(ex.message, ex);
@@ -41,23 +34,7 @@ mongoose
     useCreateIndex: true
   })
   .then(() => console.log('Connected to MongoDB...'))
-  .catch(err => console.error('Could not connect to MongoDB...'));
-
-app.set('view engine', 'pug');
-app.set('views', './views'); // default
-
-app.use(express.json()); // add middleware
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
-app.use(helmet());
-app.use('/', home);
-app.use('/api/genres', genres);
-app.use('/api/customers', customers);
-app.use('/api/movies', movies);
-app.use('/api/rentals', rentals);
-app.use('/api/users', users);
-app.use('/api/auth', auth);
-app.use(error);
+  .catch(() => console.error('Could not connect to MongoDB...'));
 
 // Configuration
 console.log('Application Name: ' + config.get('name'));
---
+++ startup/routes.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const helmet = require('helmet');
+const error = require('../middleware/error');
+const genres = require('../routes/genres');
+const customers = require('../routes/customers');
+const movies = require('../routes/movies');
+const rentals = require('../routes/rentals');
+const users = require('../routes/users');
+const auth = require('../routes/auth');
+const home = require('../routes/home');
+
+module.exports = function(app) {
+  app.set('view engine', 'pug');
+  app.set('views', './views'); // default
+
+  app.use(express.json()); // add middleware
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.static('public'));
+  app.use(helmet());
+  app.use('/', home);
+  app.use('/api/genres', genres);
+  app.use('/api/customers', customers);
+  app.use('/api/movies', movies);
+  app.use('/api/rentals', rentals);
+  app.use('/api/users', users);
+  app.use('/api/auth', auth);
+  app.use(error);
+};
